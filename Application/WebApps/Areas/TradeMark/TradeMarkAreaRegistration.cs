a50bd1fe34cccc1200f2260d8f85fbc85b82143a
sangdd <doduysang@gmail.com>
2018-05-09 00:06:07 +0700

    add file danh sach mau don

--- Application/WebApps/Areas/TradeMark/TradeMarkAreaRegistration.cs
+++ Application/WebApps/Areas/TradeMark/TradeMarkAreaRegistration.cs
@@ -1,23 +1,19 @@
-﻿using System.Web.Mvc;
-
+﻿
 namespace WebApps.Areas.TradeMark
 {
-    public class TradeMarkAreaRegistration : AreaRegistration 
-    {
-        public override string AreaName 
-        {
-            get 
-            {
-                return "TradeMark";
-            }
-        }
+    using System.Web.Mvc;
 
-        public override void RegisterArea(AreaRegistrationContext context) 
+    public class TradeMarkAreaRegistration : AreaRegistration
+    {
+        public override string AreaName => "TradeMark";
+
+        public override void RegisterArea(AreaRegistrationContext context)
         {
             context.MapRoute(
                 "TradeMark_default",
                 "TradeMark/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "WebApps.Controllers" }
             );
         }
     }
