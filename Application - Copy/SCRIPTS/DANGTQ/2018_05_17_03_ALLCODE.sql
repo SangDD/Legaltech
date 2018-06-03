028732dd6c773f3b01b8d702df1f0c1df79c30c0
SangDD <33771286+SangDD@users.noreply.github.com>
2018-06-03 14:50:59 +0700

    aa

--- Application - Copy/SCRIPTS/DANGTQ/2018_05_17_03_ALLCODE.sql
+++
@@ -1,9 +0,0 @@
-SELECT a.cdname, a.cdtype, a.cdval, a.content, a.lstodr, a.content_eng
-  FROM allcode a WHERE cdname = 'TIMESHEET' and cdtype = 'STATUS'
-  
-INSERT INTO allcode 
-VALUES('TIMESHEET','STATUS','0','Ch? duy?t',0,'New');
-INSERT INTO allcode 
-VALUES('TIMESHEET','STATUS','1','ау duy?t',1,'Approve');
-INSERT INTO allcode 
-VALUES('TIMESHEET','STATUS','2','ау t? ch?i',2,'Reject');
