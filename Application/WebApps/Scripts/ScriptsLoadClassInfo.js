c0ac2527e04ab13e84fba6a1fca25bfabadd7ec4
duyhung9981 <39828111+duyhung9981@users.noreply.github.com>
2018-06-09 00:20:01 +0700

    abc

--- Application/WebApps/Scripts/ScriptsLoadClassInfo.js
+++ Application/WebApps/Scripts/ScriptsLoadClassInfo.js
@@ -1,29 +1,46 @@
 ﻿var _timeoutcount = 500;//ms
-// textbox search
-var Nvs001txt_search_app_code = document.getElementById('Nvs001txt_search_app_code');
-// div hiển thị commbox search sau khi tìm kiếm
-var NVS002_DivShowSearchAppCode = $("#NVS002_DivShowSearchAppCode");
+// textbox search 
 
 // Init a timeout variable to be used below
 var timeout = null;
 
 // Listen for keystroke events
-Nvs001txt_search_app_code.onkeyup = function (e) {
+//Nvs001txt_search_app_code.onkeyup = function (e) {
 
-    // Clear the timeout if it has already been set.
-    // This will prevent the previous task from executing
-    // if it has been less than <MILLISECONDS>
-    clearTimeout(timeout);
+//    // Clear the timeout if it has already been set.
+//    // This will prevent the previous task from executing
+//    // if it has been less than <MILLISECONDS>
+//    clearTimeout(timeout);
 
-    // Make a new timeout set to go off in 800ms
-    timeout = setTimeout(function () {
-       // alert('Input Value:' + Nvs001txt_search_app_code.value);
-        // tìm kiếm ở đoạn này
-        NVSFunc001ShowComboxSearch()
-    }, _timeoutcount);
-};
+//    // Make a new timeout set to go off in 800ms
+//    timeout = setTimeout(function () {
+//       // alert('Input Value:' + Nvs001txt_search_app_code.value);
+//        // tìm kiếm ở đoạn này
+//        NVSFunc001ShowComboxSearch()
+//    }, _timeoutcount);
+//};
 
-function NVSFunc001ShowComboxSearch()
+function SearchAppCodeOnKeyUp(_textboxid, _divid)
+{
+    try {
+        // Clear the timeout if it has already been set.
+        // This will prevent the previous task from executing
+        // if it has been less than <MILLISECONDS>
+        clearTimeout(timeout);
+
+        // Make a new timeout set to go off in 800ms
+        timeout = setTimeout(function () {
+            // alert('Input Value:' + Nvs001txt_search_app_code.value);
+            // tìm kiếm ở đoạn này
+            NVSFunc001ShowComboxSearch(_textboxid, _divid)
+        }, _timeoutcount);
+    } catch (e) {
+        alert(e.toString())
+    }
+   
+}
+
+function NVSFunc001ShowComboxSearch(_textboxid, _divid)
 {
     try {
         if (Nvs001txt_search_app_code.value == "")
@@ -32,12 +49,12 @@ function NVSFunc001ShowComboxSearch()
             type: "POST",
             url: "/quan-ly-thong-tin/hang-hoa-dich-vu/combobox-search",
             headers: { "cache-control": "no-cache" },
-            data: { p_search: Nvs001txt_search_app_code.value },
+            data: { p_search: $("#" + _textboxid).val() },
             async: false,
             success: function (data) {
                 if (data != null) {
                     if (validateResponse(data)) {
-                        NVS002_DivShowSearchAppCode.html(data);
+                        $("#" + _divid).html(data);
                     }
                 }
                 return false;
