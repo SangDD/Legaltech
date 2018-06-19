7403844d395120991547c764b7bf1b37ccf12429
duyhung9981 <39828111+duyhung9981@users.noreply.github.com>
2018-06-20 01:20:59 +0700

    commiut

--- Application/WebApps/Areas/TradeMark/Controllers/TradeMarkShareDataController.cs
+++ Application/WebApps/Areas/TradeMark/Controllers/TradeMarkShareDataController.cs
@@ -4,11 +4,13 @@ using ObjectInfos;
 using ObjectInfos.ModuleTrademark;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.IO;
+ 
 using System.Web.Mvc;
 using WebApps.AppStart;
+using WebApps.CommonFunction;
 using WebApps.Session;
+using Common.Extensions;
 
 namespace WebApps.Areas.TradeMark.Controllers
 {
@@ -38,6 +40,74 @@ namespace WebApps.Areas.TradeMark.Controllers
                 return Json(new { success = -1 });
             }
             return Json(new { success = 0 });
+        }
+
+
+        [HttpPost]
+        [Route("get-tm04nh-info")]
+        public ActionResult TM04NHGetInfo(decimal pAppHeaderId)
+        {
+            try
+            {
+                var objBL = new AppDetail04NHBL();
+                string language = AppsCommon.GetCurrentLang();
+                var ds04NH = objBL.AppTM04NHGetByID(pAppHeaderId, language, 1);// tạm thời truyền vào trạng thái = 1
+                var _AppDetail04NHInfo = new AppDetail04NHInfo();
+                if (ds04NH != null && ds04NH.Tables.Count == 5)
+                {
+                    _AppDetail04NHInfo  = CBO<AppDetail04NHInfo>.FillObjectFromDataTable(ds04NH.Tables[0]);
+                    ViewBag.lstClassDetailInfo = CBO<AppClassDetailInfo>.FillCollectionFromDataTable(ds04NH.Tables[3]);
+                }
+                ViewBag.objAppHeaderInfo = _AppDetail04NHInfo;
+                string _viewChuDon = "";
+                string _viewDaiDienChuDon = "";
+                string _viewAppClass = "";
+                _viewChuDon = RenderPartialToString("~/Areas/TradeMark/Views/Shared/_PartialThongTinChuDon.cshtml", null);
+                _viewDaiDienChuDon = RenderPartialToString("~/Areas/TradeMark/Views/Shared/_PartialThongTinDaiDienChuDon.cshtml", null);
+                _viewAppClass = RenderPartialToString("~/Areas/TradeMark/Views/Shared/_PartialTMAddAppClass.cshtml", null);
+                return Json(new { success = 0, NgayNopDon = _AppDetail04NHInfo.Ngaynopdon_Ut.ToDateStringN0(),
+                    LogoURL = _AppDetail04NHInfo.pfileLogo,
+                    ViewChuDon = _viewChuDon, ViewDaiDienChuDon = _viewDaiDienChuDon,
+                    ViewAppClass = _viewAppClass
+                });
+
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return Json(new { success = -1 });
+            }
+            return Json(new { success = 0 });
+        }
+
+        /// <summary>
+        /// truyền vào view name và model trả ra partial dạng sstring
+        /// </summary>
+        /// <param name="viewName"></param>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public string RenderPartialToString(string viewName, object model)
+        {
+            try
+            {
+                ViewData.Model = model;
+                using (var sw = new StringWriter())
+                {
+                    var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext,
+                                                                             viewName);
+                    var viewContext = new ViewContext(ControllerContext, viewResult.View,
+                                                 ViewData, TempData, sw);
+                    viewContext.HttpContext.Response.ContentEncoding = System.Text.Encoding.GetEncoding("UTF-8");
+                    viewResult.View.Render(viewContext, sw);
+                    viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                    return sw.GetStringBuilder().ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return "";
+            }
         }
     }
 }
