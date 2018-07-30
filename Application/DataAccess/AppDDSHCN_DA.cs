a3bbe182650dbb1712aebf5bf16bdcc273a8f72c
SangDD <33771286+SangDD@users.noreply.github.com>
2018-07-30 23:54:19 +0700

    2312

---
+++ Application/DataAccess/AppDDSHCN_DA.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Data;
+using Common;
+using ObjectInfos;
+using Oracle.DataAccess.Client;
+
+namespace DataAccess
+{
+    public class AppDDSHCN_DA
+    {
+        public DataSet AppDDSHCNGetAll()
+        {
+            try
+            {
+                return OracleHelper.ExecuteDataset(Configuration.connectionString, CommandType.StoredProcedure, "PKG_APP_DDSHCN.PROC_APPDDSHCN_GETALL",
+                new OracleParameter("p_cursor", OracleDbType.RefCursor, ParameterDirection.Output));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return new DataSet();
+            }
+        }
+
+        public decimal AppDDSHCNInsert(AppDDSHCNInfo pInfo)
+        {
+            try
+            {
+                var paramReturn = new OracleParameter("p_return", OracleDbType.Int32, ParameterDirection.Output);
+                OracleHelper.ExecuteNonQuery(Configuration.connectionString, CommandType.StoredProcedure, "PKG_APP_DDSHCN.PROC_APP_DDSHCN_INSERT",
+                    new OracleParameter("P_NAME_VI", OracleDbType.Varchar2, pInfo.Name_Vi, ParameterDirection.Input),
+                    new OracleParameter("P_ADDRESS_VI", OracleDbType.Varchar2, pInfo.Address_Vi, ParameterDirection.Input),
+                    new OracleParameter("P_NAME_EN", OracleDbType.Varchar2, pInfo.Address_En, ParameterDirection.Input),
+                    new OracleParameter("P_ADDRESS_EN", OracleDbType.Varchar2, pInfo.Address_En, ParameterDirection.Input),
+                    new OracleParameter("P_PHONE", OracleDbType.Varchar2, pInfo.Phone, ParameterDirection.Input),
+                    new OracleParameter("P_FAX", OracleDbType.Varchar2, pInfo.Fax, ParameterDirection.Input),
+                    new OracleParameter("P_EMAIL", OracleDbType.Varchar2, pInfo.Email, ParameterDirection.Input),
+                    new OracleParameter("P_CREATEDDATE", OracleDbType.Date, pInfo.Createddate, ParameterDirection.Input),
+                    new OracleParameter("P_CREATEDBY", OracleDbType.Varchar2, pInfo.Createdby, ParameterDirection.Input),
+                    paramReturn);
+
+                return Convert.ToDecimal(paramReturn.Value.ToString());
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return -1;
+            }
+        }
+
+        public decimal AppDDSHCNUpdate(AppDDSHCNInfo pInfo)
+        {
+            try
+            {
+                var paramReturn = new OracleParameter("p_return", OracleDbType.Int32, ParameterDirection.Output);
+                OracleHelper.ExecuteNonQuery(Configuration.connectionString, CommandType.StoredProcedure, "PKG_APP_DDSHCN.PROC_APP_DDSHCN_UPDATE",
+                    new OracleParameter("P_ID", OracleDbType.Decimal, pInfo.Id, ParameterDirection.Input),
+                    new OracleParameter("P_NAME_VI", OracleDbType.Varchar2, pInfo.Name_Vi, ParameterDirection.Input),
+                    new OracleParameter("P_ADDRESS_VI", OracleDbType.Varchar2, pInfo.Address_Vi, ParameterDirection.Input),
+                    new OracleParameter("P_NAME_EN", OracleDbType.Varchar2, pInfo.Address_En, ParameterDirection.Input),
+                    new OracleParameter("P_ADDRESS_EN", OracleDbType.Varchar2, pInfo.Address_En, ParameterDirection.Input),
+                    new OracleParameter("P_PHONE", OracleDbType.Varchar2, pInfo.Phone, ParameterDirection.Input),
+                    new OracleParameter("P_FAX", OracleDbType.Varchar2, pInfo.Fax, ParameterDirection.Input),
+                    new OracleParameter("P_EMAIL", OracleDbType.Varchar2, pInfo.Email, ParameterDirection.Input),
+                    new OracleParameter("P_MODIFIEDDATE", OracleDbType.Date, pInfo.Modifieddate, ParameterDirection.Input),
+                    new OracleParameter("P_MODIFIEDBY", OracleDbType.Varchar2, pInfo.Modifiedby, ParameterDirection.Input),
+                    paramReturn);
+
+                return Convert.ToDecimal(paramReturn.Value.ToString());
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return -1;
+            }
+        }
+
+        public decimal AppDDSHCNDeleted(decimal pID, string pModifiedBy, DateTime pModifiedDate)
+        {
+            try
+            {
+                var paramReturn = new OracleParameter("p_return", OracleDbType.Int32, ParameterDirection.Output);
+                OracleHelper.ExecuteNonQuery(Configuration.connectionString, CommandType.StoredProcedure, "PKG_APP_DDSHCN.PROC_APP_DDSHCN_DELETED",
+                    new OracleParameter("P_ID", OracleDbType.Decimal, pID, ParameterDirection.Input),
+                    new OracleParameter("P_MODIFY_BY", OracleDbType.Varchar2, pModifiedBy, ParameterDirection.Input),
+                    new OracleParameter("P_MODIFY_DATE", OracleDbType.Date, pModifiedDate, ParameterDirection.Input),
+                    paramReturn);
+
+                return Convert.ToDecimal(paramReturn.Value.ToString());
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return -1;
+            }
+        }
+
+
+        public DataSet AppDDSHCNGetByID(decimal pID)
+        {
+            try
+            {
+                return OracleHelper.ExecuteDataset(Configuration.connectionString, CommandType.StoredProcedure, "PKG_APP_DDSHCN.PROC_APPDDSHCN_GETBYID",
+                new OracleParameter("P_ID", OracleDbType.Decimal, pID, ParameterDirection.Input),
+                new OracleParameter("p_cursor", OracleDbType.RefCursor, ParameterDirection.Output));
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return new DataSet();
+            }
+        }
+    }
+}
---
+++ Application/WebApps/Areas/DaiDienSHCN/Controllers/DDSHCNController.cs
@@ -0,0 +1,82 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApps.AppStart;
+using ObjectInfos;
+using WebApps.Session;
+using BussinessFacade;
+using Common.CommonData;
+
+
+namespace WebApps.Areas.DaiDienSHCN.Controllers
+{
+    [ValidateAntiForgeryTokenOnAllPosts]
+    [RouteArea("DDSHCN", AreaPrefix = "dai-dien-so-huu-cn")]
+    [Route("{action}")]
+    public class DDSHCNController : Controller
+    {
+        // GET: DaiDienSHCN/DDSHCN
+
+        [HttpGet]
+        [Route("danh-sach-ddsh")]
+        public ActionResult GetListDDSHCN()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return View();
+            }
+        }
+
+
+        [HttpPost]
+        [Route("danh-sach-ddsh/show-insert")]
+        public ActionResult GetView2Insert()
+        {
+            return PartialView("~/Areas/DaiDienSHCN/Views/DDSHCN/_PartialDaiDienSHCNInsert.cshtml", new AppDDSHCNInfo());
+        }
+
+        [HttpPost]
+        [Route("danh-sach-ddsh/show-update")]
+        public ActionResult GetView2Update(decimal pID)
+        {
+            return PartialView("~/Areas/DaiDienSHCN/Views/DDSHCN/_PartialDaiDienSHCNUpdate.cshtml", new AppDDSHCNInfo());
+        }
+        [HttpPost]
+        [Route("danh-sach-ddsh/show-detail")]
+        public ActionResult GetView2Detail(decimal pID)
+        {
+            return PartialView("~/Areas/DaiDienSHCN/Views/DDSHCN/_PartialDaiDienSHCNDetail.cshtml", new AppDDSHCNInfo());
+        }
+
+
+
+        [HttpPost]
+        [Route("danh-sach-ddsh/execute-insert")]
+        public ActionResult ExecuteInsert(AppDDSHCNInfo pInfo)
+        {
+            return PartialView("~/Areas/TimeSheet/Views/TimeSheet/_PartialInsertTimeSheet.cshtml", new AppDDSHCNInfo());
+        }
+
+        [HttpPost]
+        [Route("danh-sach-ddsh/execute-update")]
+        public ActionResult ExecuteUpdate(AppDDSHCNInfo pInfo)
+        {
+            return PartialView("~/Areas/TimeSheet/Views/TimeSheet/_PartialInsertTimeSheet.cshtml", new AppDDSHCNInfo());
+        }
+
+        [HttpPost]
+        [Route("danh-sach-ddsh/execute-deleted")]
+        public ActionResult ExecuteUpdate(decimal pId)
+        {
+            return PartialView("~/Areas/TimeSheet/Views/TimeSheet/_PartialInsertTimeSheet.cshtml", new AppDDSHCNInfo());
+        }
+
+
+    }
+}
