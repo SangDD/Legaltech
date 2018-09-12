f9c540906a40184b12b277d71a1eebdbf8b30268
SangDD <33771286+SangDD@users.noreply.github.com>
2018-09-12 22:19:37 +0700

    123

--- Application/BussinessFacade/ModuleArticles/NewsBL.cs
+++ Application/BussinessFacade/ModuleArticles/NewsBL.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Common;
+using DataAccess.ModuleArticles;
+using ObjectInfos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,19 @@ namespace BussinessFacade.ModuleArticles
 {
     public class NewsBL
     {
-
+        public List<NewsInfo> ArticlesGetByPage(string pLanguage, string pTitle, DateTime pNgayCongBo, int pStart, int pEnd, ref decimal pTotalRecord)
+        {
+            try
+            {
+                NewsDA _da = new NewsDA();
+                DataSet _ds = _da.ArticlesGetByPage(pLanguage,   pTitle,   pNgayCongBo,   pStart,   pEnd,ref pTotalRecord);
+                return CBO<NewsInfo>.FillCollectionFromDataSet(_ds);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return new List<NewsInfo>();
+            }
+        }
     }
 }
