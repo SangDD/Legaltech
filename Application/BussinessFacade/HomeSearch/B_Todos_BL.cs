255c2ee93ef3faf3cdb739b7d6a0b34a0aecc0d1
OS-WIN10 <OS-WIN10@DESKTOP-SRCLQQG>
2018-10-13 13:46:59 +0700

    a

--- Application/BussinessFacade/HomeSearch/B_Todos_BL.cs
+++ Application/BussinessFacade/HomeSearch/B_Todos_BL.cs
@@ -12,6 +12,23 @@ namespace BussinessFacade
 {
    public class B_Todos_BL
     {
+        public List<B_Todos_Info> Notify_Search(string p_key_search, ref decimal p_total_record,
+                string p_from = "1", string p_to = "10", string p_sort_type = "ALL")
+        {
+            try
+            {
+                B_TODOS_DA _da = new B_TODOS_DA();
+                DataSet _ds = _da.Notify_Search(p_key_search, p_from, p_to, p_sort_type, ref p_total_record);
+                return CBO<B_Todos_Info>.FillCollectionFromDataSet(_ds);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return new List<B_Todos_Info>();
+            }
+        }
+
+
         public List<B_Todos_Info> B_Todos_Search(string p_key_search, ref decimal p_total_record,
                 string p_from = "1", string p_to = "10", string p_sort_type = "ALL")
         {
