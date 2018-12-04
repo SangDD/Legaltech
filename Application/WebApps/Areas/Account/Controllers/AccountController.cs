f17aa8194ca85b440c884956608f43bebeecf877
dangtq72 <dangtq72@gmail.com>
2018-12-04 07:00:56 +0700

    dangtq sửa

--- Application/WebApps/Areas/Account/Controllers/AccountController.cs
+++ Application/WebApps/Areas/Account/Controllers/AccountController.cs
@@ -1,37 +1,38 @@
 ﻿namespace WebApps.Areas.Account.Controllers
 {
-	using System;
-	using System.Web.Mvc;
-	using AppStart;
-	using BussinessFacade;
-	using BussinessFacade.ModuleUsersAndRoles;
-	using Common;
-	using Common.CommonData;
-	using Common.Helpers;
+    using System;
+    using System.Web.Mvc;
+    using AppStart;
+    using BussinessFacade;
+    using BussinessFacade.ModuleUsersAndRoles;
+    using Common;
+    using Common.CommonData;
+    using Common.Helpers;
     using ObjectInfos.ModuleUsersAndRoles;
     using RequestFilter;
-	using Session;
+    using Session;
     using WebApps.CommonFunction;
 
 
     [ValidateAntiForgeryTokenOnAllPosts]
-	[RouteArea("Account", AreaPrefix = "")]
-	[Route("{action}")]
-	public class AccountController : Controller
-	{
-		// GET: Account/Login
-		[HttpGet][Route("login")]
-		[AllowAnonymous]
-		public ActionResult Login(string returnUrl = "")
-		{
-			if (SessionData.CurrentUser != null)
-			{
-				return this.Redirect(SessionData.CurrentUser.DefaultHomePage);
-			}
+    [RouteArea("Account", AreaPrefix = "")]
+    [Route("{action}")]
+    public class AccountController : Controller
+    {
+        // GET: Account/Login
+        [HttpGet]
+        [Route("login")]
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl = "")
+        {
+            if (SessionData.CurrentUser != null)
+            {
+                return this.Redirect(SessionData.CurrentUser.DefaultHomePage);
+            }
 
-			ViewBag.returnUrl = returnUrl;
-			return View();
-		}
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
 
         [HttpPost]
         [Route("register")]
@@ -42,7 +43,7 @@
             try
             {
                 UserBL objBL = new UserBL();
-                int pReturn  = objBL.RegisterInsert(pRegisInfo);
+                int pReturn = objBL.RegisterInsert(pRegisInfo);
                 return Json(new { status = pReturn });
             }
             catch (Exception ex)
@@ -52,53 +53,55 @@
             }
         }
 
-        [HttpPost][Route("dang-nhap")]
-		[AllowAnonymous]
-		public ActionResult Login(string userName, string password, string returnUrl = "")
-		{
-			if (SessionData.CurrentUser != null)
-			{
-				return Json(new { redirectTo = SessionData.CurrentUser.DefaultHomePage });
-			}
+        [HttpPost]
+        [Route("dang-nhap")]
+        [AllowAnonymous]
+        public ActionResult Login(string userName, string password, string returnUrl = "")
+        {
+            if (SessionData.CurrentUser != null)
+            {
+                return Json(new { redirectTo = SessionData.CurrentUser.DefaultHomePage });
+            }
             string language = AppsCommon.GetCurrentLang();
             var result = new ActionBusinessResult();
-			try
+            try
             {
                 var userBL = new UserBL();
-				result = userBL.DoLoginAccount(userName, password, language);
-				if (result.IsActionSuccess)
-				{
-					var ipAddress = HttpHelper.GetClientIPAddress(System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"]);
-					FileHelper.WriteFileLogin(CommonVariables.KnFileLogin, userName, ipAddress);
+                result = userBL.DoLoginAccount(userName, password, language);
+                if (result.IsActionSuccess)
+                {
+                    var ipAddress = HttpHelper.GetClientIPAddress(System.Web.HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"]);
+                    FileHelper.WriteFileLogin(CommonVariables.KnFileLogin, userName, ipAddress);
                     userBL.CurrentUserInfo.Language = language;
                     SessionData.CurrentUser = userBL.CurrentUserInfo;
-					SessionData.CurrentUser.DefaultHomePage = IdentityRequest.GetDefaultPageForAccountLogged();
+                    SessionData.CurrentUser.DefaultHomePage = IdentityRequest.GetDefaultPageForAccountLogged();
                     SessionData.CurrentUser.CurrentDate = CommonFuc.TruncDate();
 
                     var urlContinue = SessionData.CurrentUser.DefaultHomePage;
-					if (!string.IsNullOrEmpty(returnUrl)) urlContinue = returnUrl;
-                    if (userBL.CurrentUserInfo.loginfirst == 0 && userName!= "SuperAdmin")
+                    if (!string.IsNullOrEmpty(returnUrl)) urlContinue = returnUrl;
+                    if (userBL.CurrentUserInfo.loginfirst == 0 && userName != "SuperAdmin")
                     {
-                        urlContinue = "/quan-tri-he-thong/quan-ly-nguoi-dung/get-view-to-edit-user/" + userBL.CurrentUserInfo.Id.ToString();
+                        urlContinue = "/customer/quan-ly-customer/get-view-to-edit-customer/" + userBL.CurrentUserInfo.Id.ToString();
                     }
-                        return Json(new { result = result.ToJson(), urlContinue });
-				}
-			}
-			catch (Exception ex)
-			{
-				Logger.LogException(ex);
-			}
+                    return Json(new { result = result.ToJson(), urlContinue });
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
 
-			return Json(new { result = result.ToJson() });
-		}
+            return Json(new { result = result.ToJson() });
+        }
 
-		[AllowAnonymous]
-		[HttpGet][Route("dang-xuat")]
-		public ActionResult Logout()
-		{
-			SessionData.CurrentUser = null;
-			Session.Abandon();
-			return this.Redirect("/");
-		}
-	}
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("dang-xuat")]
+        public ActionResult Logout()
+        {
+            SessionData.CurrentUser = null;
+            Session.Abandon();
+            return this.Redirect("/");
+        }
+    }
 }
