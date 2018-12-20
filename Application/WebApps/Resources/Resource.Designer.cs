94d6799c6cc5bc95e3c6342d7e1edc3251e1bbb2
dangtq72 <dangtq72@gmail.com>
2018-12-20 22:21:25 +0700

    a

--- Application/WebApps/Resources/Resource.Designer.cs
+++ Application/WebApps/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace WebApps.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -682,6 +682,15 @@ namespace WebApps.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Đơn khiếu nại.
+        /// </summary>
+        public static string lblKhieuNai {
+            get {
+                return ResourceManager.GetString("lblKhieuNai", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Không.
         /// </summary>
         public static string lblKhong {
@@ -1011,6 +1020,15 @@ namespace WebApps.Resources {
         public static string lblNoiDung {
             get {
                 return ResourceManager.GetString("lblNoiDung", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Nộp đơn.
+        /// </summary>
+        public static string lblNopdon {
+            get {
+                return ResourceManager.GetString("lblNopdon", resourceCulture);
             }
         }
         
