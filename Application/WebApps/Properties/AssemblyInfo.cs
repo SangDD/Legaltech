c5a4e35e391bee958c15f3bc6c45328d9a72829e
duyhung9981 <39828111+duyhung9981@users.noreply.github.com>
2019-02-19 00:53:03 +0700

    222

--- Application/WebApps/Properties/AssemblyInfo.cs
+++ Application/WebApps/Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.20.00.00")]
+[assembly: AssemblyVersion("1.21.00.00")]
