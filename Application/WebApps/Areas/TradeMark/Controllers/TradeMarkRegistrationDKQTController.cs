b3b3a84bc928803e133e3db2bdcddc04f053a739
dangtq72 <dangtq72@gmail.com>
2019-02-18 23:56:59 +0700

    dangtq

--- Application/WebApps/Areas/TradeMark/Controllers/TradeMarkRegistrationDKQTController.cs
+++ Application/WebApps/Areas/TradeMark/Controllers/TradeMarkRegistrationDKQTController.cs
@@ -242,7 +242,7 @@
                 AppDetail06DKQT_BL objDetail = new AppDetail06DKQT_BL();
                 AppDocumentBL objDoc = new AppDocumentBL();
                 AppClassDetailBL objClassDetail = new AppClassDetailBL();
-
+                bool _IsOk = false;
                 if (pInfo == null || pDetail == null) return Json(new { status = ErrorCode.Error });
                 string language = AppsCommon.GetCurrentLang();
                 var CreatedBy = SessionData.CurrentUser.Username;
@@ -361,6 +361,25 @@
                     else
                     {
                         scope.Complete();
+                        _IsOk = true;
+                    }
+                }
+
+                // tự động update todo
+                if (pInfo.UpdateToDo == 1 && _IsOk == true)
+                {
+                    if (pInfo.Status == (int)CommonEnums.App_Status.ChoKHConfirm)
+                    {
+                        Application_Header_BL _obj_bl = new Application_Header_BL();
+                        decimal _status = (decimal)CommonEnums.App_Status.KhacHangDaConfirm;
+
+                        string _note = "Xác nhận nộp đơn";
+                        if (AppsCommon.GetCurrentLang() != "VI_VN")
+                        {
+                            _note = "confirmation for filing";
+                        }
+                        int _ck = _obj_bl.AppHeader_Update_Status(pInfo.Case_Code, _status, _note,
+                            SessionData.CurrentUser.Username, DateTime.Now, AppsCommon.GetCurrentLang());
                     }
                 }
                 return Json(new { status = pInfo.Id });
