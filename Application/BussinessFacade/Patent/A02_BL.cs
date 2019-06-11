38bb9c67242e77c1b28b528fe0620f2a846dcb58
HuyHung <duyhung9981@gmail.com>
2019-06-11 21:18:12 +0700

    aaaa

---
+++ Application/BussinessFacade/Patent/A02_BL.cs
@@ -0,0 +1,90 @@
+﻿using Common;
+using ObjectInfos;
+using System;
+using DataAccess;
+using ObjectInfos.ModuleTrademark;
+using System.Collections.Generic;
+using System.Data;
+
+namespace BussinessFacade
+{
+    public class A02_BL
+    {
+        public decimal Insert(A02_Info pInfo)
+        {
+            try
+            {
+                A02_DA _obj_da = new A02_DA();
+                return _obj_da.Insert(pInfo);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return ErrorCode.Error;
+            }
+        }
+
+
+        public decimal UpDate(A02_Info pInfo)
+        {
+            try
+            {
+                A02_DA _obj_da = new A02_DA();
+                return _obj_da.UpDate(pInfo);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return ErrorCode.Error;
+            }
+        }
+
+        public int Deleted(decimal p_app_header_id, string pAppCode, string pLanguage)
+        {
+            try
+            {
+                A02_DA _obj_da = new A02_DA();
+                return _obj_da.Deleted(p_app_header_id, pAppCode, pLanguage);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return ErrorCode.Error;
+            }
+        }
+
+        public A02_Info GetByID(decimal p_app_header_id, string p_language_code,
+            ref ApplicationHeaderInfo applicationHeaderInfo,
+            ref List<AppDocumentInfo> appDocumentInfos, ref List<AppFeeFixInfo> appFeeFixInfos,
+            ref List<AuthorsInfo> pAppAuthorsInfo, ref List<Other_MasterInfo> pOther_MasterInfo,
+            ref List<AppClassDetailInfo> appClassDetailInfos, ref List<AppDocumentOthersInfo> pAppDocOtherInfo, ref List<UTienInfo> pUTienInfo)
+        {
+            try
+            {
+                A02_DA _obj_da = new A02_DA();
+                DataSet dataSet = _obj_da.GetByID(p_app_header_id, p_language_code);
+                A02_Info _A02_Info = CBO<A02_Info>.FillObjectFromDataSet(dataSet);
+                if (dataSet != null && dataSet.Tables.Count == 9)
+                {
+                    applicationHeaderInfo = CBO<ApplicationHeaderInfo>.FillObjectFromDataTable(dataSet.Tables[1]);
+                    appDocumentInfos = CBO<AppDocumentInfo>.FillCollectionFromDataTable(dataSet.Tables[2]);
+                    appFeeFixInfos = CBO<AppFeeFixInfo>.FillCollectionFromDataTable(dataSet.Tables[3]);
+                    pOther_MasterInfo = CBO<Other_MasterInfo>.FillCollectionFromDataTable(dataSet.Tables[4]);
+                    pAppAuthorsInfo = CBO<AuthorsInfo>.FillCollectionFromDataTable(dataSet.Tables[5]);
+
+                    appClassDetailInfos = CBO<AppClassDetailInfo>.FillCollectionFromDataTable(dataSet.Tables[6]);
+                    pAppDocOtherInfo = CBO<AppDocumentOthersInfo>.FillCollectionFromDataTable(dataSet.Tables[7]);
+                    pUTienInfo = CBO<UTienInfo>.FillCollectionFromDataTable(dataSet.Tables[8]);
+                }
+
+                return _A02_Info;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+                return new A02_Info();
+            }
+        }
+
+    }
+}
---
+++ Application/ObjectInfo/Patent/A02_Info.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInfos
+{
+    /// <summary>
+    /// Dùng chung cho một số info khác 
+    /// </summary>
+    public class A02_Info : ApplicationHeaderInfo
+    {
+        public decimal A02_Id { set; get; }
+        public decimal Used_Special { set; get; }
+
+        //public string Case_Code { get; set; }
+        public decimal App_Header_Id { get; set; }
+        public string Language_Code { get; set; }
+        public string Appno { get; set; }
+        public string Patent_Type { get; set; }
+        public string Patent_Name { get; set; }
+        public string Source_PCT { get; set; }
+        public string PCT_Number { get; set; }
+        public DateTime PCT_Filling_Date_Qt { get; set; }
+        public string PCT_Number_Qt { get; set; }
+        public DateTime PCT_Date { get; set; }
+        public DateTime PCT_VN_Date { get; set; }
+        public decimal PCT_Suadoi { get; set; }
+        public string PCT_Suadoi_Type { get; set; }
+        public string PCT_Suadoi_Content { get; set; }
+        public string Source_DQSC { get; set; }
+        public string DQSC_Origin_App_No { get; set; }
+        public DateTime DQSC_Filling_Date { get; set; }
+        public decimal DQSC_Valid_Before { get; set; }
+        public decimal DQSC_Valid_After { get; set; }
+        public string Source_GPHI { get; set; }
+        public string GPHI_Origin_App_No { get; set; }
+        public DateTime GPHI_Filling_Date { get; set; }
+        public decimal GPHI_Valid_Before { get; set; }
+        public decimal GPHI_Valid_After { get; set; }
+
+        public string ThamDinhNoiDung { get; set; }
+        public string ChuyenDoiDon { get; set; }
+        public decimal Point { get; set; }
+
+        public string Class_Type { get; set; }
+        public string Class_Content { get; set; }
+
+    }
+}
