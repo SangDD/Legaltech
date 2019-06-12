0ff7a09c90dbdd8201601539b0516c9a1c94944e
HuyHung <duyhung9981@gmail.com>
2019-06-12 23:13:58 +0700

    a

--- Application/WebApps/Resources/Resource.Designer.cs
+++ Application/WebApps/Resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace WebApps.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -1236,6 +1236,15 @@ namespace WebApps.Resources {
         public static string HoTen {
             get {
                 return ResourceManager.GetString("HoTen", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Đăng ký kiểu dáng công nghiệp.
+        /// </summary>
+        public static string IndusDesign6a_Title {
+            get {
+                return ResourceManager.GetString("IndusDesign6a_Title", resourceCulture);
             }
         }
         
