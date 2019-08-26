1fcdb568e6a38ccc8f68fe74b547d300138bf78e
hatm72 <Lucy0751060770!@#0802>
2019-08-26 21:52:19 +0700

    cm

--- Application/WebApps/Areas/Patent/Controllers/B03Controller.cs
+++ Application/WebApps/Areas/Patent/Controllers/B03Controller.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApps.AppStart;
 
 namespace WebApps.Areas.Patent.Controllers
 {
+    [ValidateAntiForgeryTokenOnAllPosts]
+    [RouteArea("PatentRegistration", AreaPrefix = "lg-patentB03")]
+    [Route("{action}")]
     public class B03Controller : Controller
     {
-        // GET: Patent/B03
+        [HttpGet]
+        [Route("register/{id}")]
         public ActionResult B03Display()
         {
             return View();
