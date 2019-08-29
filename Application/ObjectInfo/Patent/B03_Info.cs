55c3c5411823e03faa27d3247b2181d2c0a8fa10
hatm72 <Lucy0751060770!@#0802>
2019-08-29 20:30:46 +0700

    cm

---
+++ Application/ObjectInfo/Patent/B03_Info.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectInfos
+{
+    public class B03_Info
+    {
+        public decimal ID { set; get; }
+        public string case_code { get; set; }
+        public decimal App_Header_Id { get; set; }
+        public string Language_Code { get; set; }
+        public string app_detail_number { get; set; }
+        public string name_evaluator { get; set; }
+        public string address_evaluator { get; set; }
+        public string phone_evaluator { get; set; }
+        public string fax_evaluator { get; set; }
+        public string email_evaluator { get; set; }
+        public string type_evaluator { get; set; }
+        public string point { get; set; }
+        public string thamdinhnoidung { get; set; }
+    }
+}
