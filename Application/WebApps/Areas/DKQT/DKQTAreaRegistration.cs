9db35d5f9a7f92e29bdcece035aab4262e37d403
HuyHung <duyhung9981@gmail.com>
2019-09-11 21:13:52 +0700

    h

---
+++ Application/WebApps/Areas/DKQT/DKQTAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebApps.Areas.DKQT
+{
+    public class DKQTAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DKQT";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DKQT_default",
+                "DKQT/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
