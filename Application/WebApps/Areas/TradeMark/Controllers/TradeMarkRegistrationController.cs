d788b18381c6021479ff9ea338aeb77c8c029b11
hatm72 <Lucy0751060770!@#0802>
2019-09-21 23:55:28 +0700

    CM

--- Application/WebApps/Areas/TradeMark/Controllers/TradeMarkRegistrationController.cs
+++ Application/WebApps/Areas/TradeMark/Controllers/TradeMarkRegistrationController.cs
@@ -1852,6 +1852,22 @@
                 ViewBag.Isdisable = 0;
                 return PartialView(@"~\Areas\DKQT\Views\C07\_Partial_C07_Edit.cshtml");
             }
+            else if(pAppCode == TradeMarkAppCode.AppCode_E01)
+            {
+               
+                    var objBL = new BussinessFacade.ModuleTrademark.Application_Header_BL();
+                    string language = AppsCommon.GetCurrentLang();
+                    List<AppDocumentInfo> appDocumentInfos = new List<AppDocumentInfo>();
+                    List<AppFeeFixInfo> _lst_appFeeFixInfos = new List<AppFeeFixInfo>();
+                    ApplicationHeaderInfo applicationHeaderInfo = new ApplicationHeaderInfo();
+                    ApplicationHeaderInfo _app = objBL.GetAllByID(pAppHeaderId, language, ref applicationHeaderInfo, ref appDocumentInfos, ref _lst_appFeeFixInfos);
+                    ViewBag.Lst_AppDoc = appDocumentInfos;
+                    ViewBag.Lst_AppFee = _lst_appFeeFixInfos;
+                    ViewBag.objAppHeaderInfo = applicationHeaderInfo;
+
+                    return PartialView("~/Areas/Patent/Views/E01/_Partial_E01_Edit.cshtml");
+               
+            }
             else
             {
                 //
