b0b4e2d91e889c274065fcf85bb5143cb78fc4ab
hatm72 <Lucy0751060770!@#0802>
2019-09-23 22:58:32 +0700

    CM

---
+++ Application/WebApps/Areas/TradeMark/Controllers/C02Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApps.AppStart;
+
+namespace WebApps.Areas.TradeMark.Controllers
+{
+    [ValidateAntiForgeryTokenOnAllPosts]
+    [RouteArea("PatentRegistration", AreaPrefix = "trade-mark-c02")]
+    [Route("{action}")]
+    public class C02Controller : Controller
+    {
+        [HttpGet]
+        [Route("register/{id}")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
