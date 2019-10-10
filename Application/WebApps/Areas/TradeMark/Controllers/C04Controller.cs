2844fa4a8d688c82ea8f35998d9768da5df24089
hatm72 <Lucy0751060770!@#0802>
2019-10-10 21:54:51 +0700

    CM

---
+++ Application/WebApps/Areas/TradeMark/Controllers/C04Controller.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApps.AppStart;
+using WebApps.Session;
+
+namespace WebApps.Areas.TradeMark.Controllers
+{
+    [ValidateAntiForgeryTokenOnAllPosts]
+    [RouteArea("PatentRegistration", AreaPrefix = "trade-mark-c04")]
+    [Route("{action}")]
+    public class C04Controller : Controller
+    {
+        [HttpGet]
+        [Route("register/{id}")]
+        public ActionResult Register()
+        {
+            try
+            {
+                if (SessionData.CurrentUser == null)
+                    return Redirect("/");
+
+                SessionData.CurrentUser.chashFile.Clear();
+                string AppCode = "";
+                if (RouteData.Values.ContainsKey("id"))
+                {
+                    AppCode = RouteData.Values["id"].ToString().ToUpper();
+                }
+                ViewBag.AppCode = AppCode;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex);
+            }
+            return PartialView("~/Areas/TradeMark/Views/C02/_Partial_C04_Register.cshtml");
+
+        }
+    }
+}
