0112df9bea0599e0862374318c133669946a3c2b
ADMIN <ADMIN@DESKTOP-70BPNA4>
2020-02-20 21:17:09 +0700

    cm

--- Application/WebApps/Areas/TradeMark/Controllers/TradeMarkRegistrationController.cs
+++ Application/WebApps/Areas/TradeMark/Controllers/TradeMarkRegistrationController.cs
@@ -2127,6 +2127,30 @@
 
                 return PartialView("~/Areas/TradeMark/Views/C02/_Partial_C02_View.cshtml");
             }
+            if (pAppCode == TradeMarkAppCode.AppCodeDangKynhanHieuNN)
+            {
+                var objBL = new App_Detail_F04_BL();
+                string language = AppsCommon.GetCurrentLang();
+                ApplicationHeaderInfo _applicationheaderinfo = new ApplicationHeaderInfo();
+                List<AppDocumentInfo> _lst_appdocument = new List<AppDocumentInfo>();
+                List<AppDocumentOthersInfo> _lst_appdocumentothers = new List<AppDocumentOthersInfo>();
+                App_Detail_F04_Info ds04NH = objBL.GetByID(pAppHeaderId, language, ref _applicationheaderinfo, ref _lst_appdocument, ref _lst_appdocumentothers);
+
+                //Luu key duy nhat cua he thong
+                string keyData = "objAppHeaderInfo" + SessionData.CurrentUser.Id.ToString() + DateTime.Now.ToString("DDMMHHmmss");
+                ViewBag.keyData = keyData;
+                SessionData.SetDataSession(keyData, "");
+
+                if (ds04NH != null)
+                {
+                    ViewBag.objAppHeaderInfo = ds04NH;
+                    SessionData.SetDataSession(keyData, ViewBag.objAppHeaderInfo);
+                    ViewBag.lstDocumentInfo = _lst_appdocument;
+                    ViewBag.lstDocOther = _lst_appdocumentothers;
+
+                }
+                return PartialView("~/Areas/TradeMark/Views/TradeMarkRegistration/View_PartialDangKyNhanHieuNN.cshtml");
+            }
             else
             {
                 return PartialView("~/Areas/TradeMark/Views/TradeMarkRegistration/View_PartialDangKyNhanHieu.cshtml");
